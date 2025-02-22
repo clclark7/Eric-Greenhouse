56e3a14ac647bb33e2259f8110a96ae59f0bcef7
Andrew Clark <akclark4@ncsu.edu>
2025-02-22 01:35:25 +0000

    feat: restructure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+import { Router } from "./Router";
+import { theme } from "./theme";
+
+function App() {
+  return (
+    <>
+      <MantineProvider theme={theme}>
+        <Router />
+      </MantineProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard.page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+]);
+
+export function Router() {
+  return <RouterProvider router={router} />;
+}
